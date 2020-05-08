3133ab85ba34ad1830ab21112f722edaaeee6c94
publicgiraffe <zabavnoahah@gmail.com>
2020-05-08 22:09:48 +0300

    session fix

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -27,6 +27,7 @@ namespace WebApplication1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDistributedMemoryCache();
             services.AddControllersWithViews();
             services.AddLocalization(opts => { opts.ResourcesPath = "Resources"; });
             services.AddSession(options =>
